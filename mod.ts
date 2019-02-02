d92a210f48d30bed3e04add5f6db0c74bd7fe308
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-02-02 10:40:46 +0100

    prettier

--- mod.ts
+++ mod.ts
@@ -1,117 +1,137 @@
 interface _bigint {
-  l: number
-  h: number
+  l: number;
+  h: number;
 }
 
 function _add(a: _bigint, b: _bigint): void {
-    var rl: number = a.l + b.l
-let  a2: _bigint = { h: a.h + b.h + (rl / 2 >>> 31) >>> 0,
-               l: rl >>> 0 };
-    a.h = a2.h; a.l = a2.l;
+  const rl: number = a.l + b.l;
+  const a2: _bigint = { h: (a.h + b.h + ((rl / 2) >>> 31)) >>> 0, l: rl >>> 0 };
+  a.h = a2.h;
+  a.l = a2.l;
 }
 
 function _xor(a: _bigint, b: _bigint): void {
-    a.h ^= b.h; a.h >>>= 0;
-    a.l ^= b.l; a.l >>>= 0;
+  a.h ^= b.h;
+  a.h >>>= 0;
+  a.l ^= b.l;
+  a.l >>>= 0;
 }
 
 function _rotl(a: _bigint, n: number): void {
-    var a2: _bigint = {
-        h: a.h << n | a.l >>> (32 - n),
-        l: a.l << n | a.h >>> (32 - n)
-    };
-    a.h = a2.h; a.l = a2.l;
+  const a2: _bigint = {
+    h: (a.h << n) | (a.l >>> (32 - n)),
+    l: (a.l << n) | (a.h >>> (32 - n))
+  };
+  a.h = a2.h;
+  a.l = a2.l;
 }
 
 function _rotl32(a: _bigint): void {
-    var al: number = a.l;
-    a.l = a.h; a.h = al;
+  const al: number = a.l;
+  a.l = a.h;
+  a.h = al;
 }
 
-function _compress(v0:_bigint, v1: _bigint, v2: _bigint, v3: _bigint): void {
-    _add(v0, v1);
-    _add(v2, v3);
-    _rotl(v1, 13);
-    _rotl(v3, 16);
-    _xor(v1, v0);
-    _xor(v3, v2);
-    _rotl32(v0);
-    _add(v2, v1);
-    _add(v0, v3);
-    _rotl(v1, 17);
-    _rotl(v3, 21);
-    _xor(v1, v2);
-    _xor(v3, v0);
-    _rotl32(v2);
+function _compress(v0: _bigint, v1: _bigint, v2: _bigint, v3: _bigint): void {
+  _add(v0, v1);
+  _add(v2, v3);
+  _rotl(v1, 13);
+  _rotl(v3, 16);
+  _xor(v1, v0);
+  _xor(v3, v2);
+  _rotl32(v0);
+  _add(v2, v1);
+  _add(v0, v3);
+  _rotl(v1, 17);
+  _rotl(v3, 21);
+  _xor(v1, v2);
+  _xor(v3, v0);
+  _rotl32(v2);
 }
 
-function _get_uint32(a: Uint8Array, offset: number): number {
-    return a[offset + 3] << 24 |
-           a[offset + 2] << 16 |
-           a[offset + 1] << 8 |
-           a[offset];
+function _getuint32(a: Uint8Array, offset: number): number {
+  return (
+    (a[offset + 3] << 24) |
+    (a[offset + 2] << 16) |
+    (a[offset + 1] << 8) |
+    a[offset]
+  );
 }
 
 function _to4uint32s(a: Uint8Array): number[] {
-    return [_get_uint32(a, 0), _get_uint32(a, 4),
-            _get_uint32(a, 8), _get_uint32(a, 12)];
+  return [
+    _getuint32(a, 0),
+    _getuint32(a, 4),
+    _getuint32(a, 8),
+    _getuint32(a, 12)
+  ];
 }
 
-function _to8Bytes(h: _bigint): Uint8Array {
-  const dv: DataView = new DataView(new ArrayBuffer(8));
+function _8BytesBEfromBigintLE(h: _bigint, out: Uint8Array): void {
+  const dv: DataView = new DataView(out.buffer);
   dv.setUint32(0, h.h, true); // offset, num, littleEndian
   dv.setUint32(4, h.l, true);
-  return new Uint8Array(dv.buffer);
 }
 
-export function siphash24(key: Uint8Array, msg: Uint8Array): Uint8Array {
-    if (key.length !== 16) {
-      throw TypeError("key.length is not 16 bytes");
-    }
-    let k: number[] = _to4uint32s(key);
-    var k0: _bigint = { h: k[1] >>> 0, l: k[0] >>> 0 };
-        let k1: _bigint = { h: k[3] >>> 0, l: k[2] >>> 0 };
-        let v0: _bigint = { h: k0.h, l: k0.l }; let v2:_bigint = k0;
-        let v1: _bigint = { h: k1.h, l: k1.l }; let v3: _bigint = k1;
-        let mi: _bigint; let mp: number = 0; let ml: number = msg.length; let ml7: number = ml - 7;
-        let buf: Uint8Array = new Uint8Array(new ArrayBuffer(8)); // rid
-
-    _xor(v0, { h: 0x736f6d65, l: 0x70736575 });
-    _xor(v1, { h: 0x646f7261, l: 0x6e646f6d });
-    _xor(v2, { h: 0x6c796765, l: 0x6e657261 });
-    _xor(v3, { h: 0x74656462, l: 0x79746573 });
-    while (mp < ml7) {
-        mi = { h: _get_uint32(msg, mp + 4), l: _get_uint32(msg, mp) };
-        _xor(v3, mi);
-        _compress(v0, v1, v2, v3);
-        _compress(v0, v1, v2, v3);
-        _xor(v0, mi);
-        mp += 8;
-    }
-    buf[7] = ml;
-    var ic: number = 0;
-    while (mp < ml) {
-        buf[ic++] = msg[mp++];
-    }
-    while (ic < 7) {
-        buf[ic++] = 0;
-    }
-    mi = { h: buf[7] << 24 | buf[6] << 16 | buf[5] << 8 | buf[4],
-           l: buf[3] << 24 | buf[2] << 16 | buf[1] << 8 | buf[0] };
+export function siphash24(
+  msg: Uint8Array,
+  key: Uint8Array,
+  out: Uint8Array
+): void {
+  if (key.length !== 16) {
+    throw TypeError("key.length is not 16 bytes");
+  }
+  if (out.length !== 8) {
+    throw TypeError("out.length is not 8 bytes");
+  }
+  const k: number[] = _to4uint32s(key);
+  const k0: _bigint = { h: k[1] >>> 0, l: k[0] >>> 0 };
+  const k1: _bigint = { h: k[3] >>> 0, l: k[2] >>> 0 };
+  const v0: _bigint = { h: k0.h, l: k0.l };
+  const v2: _bigint = k0;
+  const v1: _bigint = { h: k1.h, l: k1.l };
+  const v3: _bigint = k1;
+  let mi: _bigint;
+  let mp: number = 0;
+  const ml: number = msg.length;
+  const ml7: number = ml - 7;
+  const buf: Uint8Array = new Uint8Array(8);
+  _xor(v0, { h: 0x736f6d65, l: 0x70736575 });
+  _xor(v1, { h: 0x646f7261, l: 0x6e646f6d });
+  _xor(v2, { h: 0x6c796765, l: 0x6e657261 });
+  _xor(v3, { h: 0x74656462, l: 0x79746573 });
+  while (mp < ml7) {
+    mi = { h: _getuint32(msg, mp + 4), l: _getuint32(msg, mp) };
     _xor(v3, mi);
     _compress(v0, v1, v2, v3);
     _compress(v0, v1, v2, v3);
     _xor(v0, mi);
-    _xor(v2, { h: 0, l: 0xff });
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-    _compress(v0, v1, v2, v3);
-
-    var h: _bigint = v0;
-    _xor(h, v1);
-    _xor(h, v2);
-    _xor(h, v3);
-
-    return _to8Bytes(h);
+    mp += 8;
+  }
+  buf[7] = ml;
+  let ic: number = 0;
+  while (mp < ml) {
+    buf[ic++] = msg[mp++];
+  }
+  while (ic < 7) {
+    buf[ic++] = 0;
+  }
+  mi = {
+    h: (buf[7] << 24) | (buf[6] << 16) | (buf[5] << 8) | buf[4],
+    l: (buf[3] << 24) | (buf[2] << 16) | (buf[1] << 8) | buf[0]
+  };
+  _xor(v3, mi);
+  _compress(v0, v1, v2, v3);
+  _compress(v0, v1, v2, v3);
+  _xor(v0, mi);
+  _xor(v2, { h: 0, l: 0xff });
+  _compress(v0, v1, v2, v3);
+  _compress(v0, v1, v2, v3);
+  _compress(v0, v1, v2, v3);
+  _compress(v0, v1, v2, v3);
+  const h: _bigint = v0;
+  _xor(h, v1);
+  _xor(h, v2);
+  _xor(h, v3);
+  _8BytesBEfromBigintLE(h, out);
 }
--- test.ts
+++ test.ts
@@ -1,15 +1,27 @@
 import { readFileSync } from "deno";
-import { parseTestVector } from "./util.ts";
 import { siphash24 } from "./mod.ts";
 import { test, equal, runTests } from "https://deno.land/x/testing/mod.ts";
 
 const testVectors = JSON.parse(
   new TextDecoder().decode(readFileSync("./test_vectors.json"))
-).map(parseTestVector);
+).map(function parseTestVector(v: {
+  key: number[];
+  msg: number[];
+  expected: number[];
+}): { key: Uint8Array; msg: Uint8Array; expected: Uint8Array } {
+  return {
+    key: new Uint8Array(v.key),
+    msg: new Uint8Array(v.msg),
+    expected: new Uint8Array(v.expected)
+  };
+});
 
 test(function passesTestVectors(): void {
+  const out: Uint8Array = new Uint8Array(8);
   for (const testVector of testVectors) {
-    equal(siphash24(testVector.key, testVector.msg), testVector.expected);
+    siphash24(testVector.msg, testVector.key, out);
+    equal(out, testVector.expected);
+    out.fill(0);
   }
 });
 
